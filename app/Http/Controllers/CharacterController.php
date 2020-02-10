95281bf97fc42cceb544d5b78455fbac540de664
Tom Stanley <tom.stanley37@gmail.com>
2020-02-10 19:08:24 +0000

    Switch to React for UI and get registration working

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -7,81 +7,81 @@ use Illuminate\Http\Request;
 
 class CharacterController extends Controller
 {
-    /**
-     * Display a list of all characters.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $characters = Character::all();
+	/**
+	 * Display a list of all characters.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$characters = Character::all();
 
-        return view("characters.index", ["characters" => $characters]);
-    }
+		return view("characters.index", ["characters" => $characters]);
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		//
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Character  $character
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Character $character)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Character  $character
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Character $character)
+	{
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Character  $character
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Character $character)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Character  $character
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Character $character)
+	{
+		//
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Character  $character
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Character $character)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Character  $character
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, Character $character)
+	{
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Character  $character
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Character $character)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Character  $character
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Character $character)
+	{
+		//
+	}
 }
